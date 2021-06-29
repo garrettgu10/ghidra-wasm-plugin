d2e30a1b1bdb3268b9f72d3327792653d956dff9
Garrett Gu <garrettgu777@gmail.com>
2021-06-29 13:42:00 -0500

    new Leb128 generator script

--- data/languages/codegen/Leb128Gen.py
+++ data/languages/codegen/Leb128Gen.py
@@ -8,8 +8,8 @@ print(";\n")
 
 for i in range(1, MAX_BYTES+1):
     definition = "ULeb128: val is "
-    for j in range(i-1, -1, -1):
-        definition += "topbit = " + str(0 if j == 0 else 1) + " & v" + str(j) + (" " if j == 0 else "; ")
+    for j in range(0, i):
+        definition += "topbit = " + str(0 if j == i-1 else 1) + " & v" + str(j) + (" " if j == i-1 else "; ")
     print(definition)
     valdef = "\t[ val = 0 | v0 "+ ("; " if i==1 else "| ")
     for j in range(1, i):
