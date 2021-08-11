36ca7ca392c6f3b88f98bb5102800d86eb37d170
Garrett Gu <garrettgu777@gmail.com>
2021-08-11 12:36:21 -0500

    add negative Leb128 handling, br_table, memory grow and size

--- data/languages/codegen/Leb128Gen.py
+++ data/languages/codegen/Leb128Gen.py
@@ -18,3 +18,17 @@ for i in range(1, MAX_BYTES+1):
     
     print(valdef)
     print("{ res:8 = val; export res; }")
+
+for i in range(1, MAX_BYTES+1):
+    definition = "SLeb128: val is "
+    for j in range(0, i):
+        definition += "topbit = " + str(0 if j == i-1 else 1) + " & v" + str(j) + (" " if j == i-1 else "; ")
+    print(definition)
+    valdef = "\t[ val = ((0 | v0 "+ ("" if i==1 else "| ")
+    sext_val = "(1 << " + str(7 * i - 1) + ")" if (7 * i < 64) else "0"
+    for j in range(1, i):
+        valdef += "( v" + str(j) + " << " + str(7 * j) + " ) " + ("" if j==i-1 else "| ")
+    valdef += ") ^ " + sext_val + ") - " + sext_val + "; ]"
+    
+    print(valdef)
+    print("{ res:8 = val; export res; }")
